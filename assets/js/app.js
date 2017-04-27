4f91b5c9c26c02a8e0f9e957a9843771d9c50cc2
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-27 17:17:34 -0500

    creando js - validacion fecha

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var dato = document.getElementsByClassName("input")[3];
+document.getElementsByClassName("button")[0].addEventListener("click",function () {
+})
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1 ;
+var year = date.getFullYear();
+
+// var n = dato.value.split("-");

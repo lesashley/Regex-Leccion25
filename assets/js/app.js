ce920a60c22b720e58232d21a807a97162259110
lesashley <lesashley56@gmail.com>
2017-04-30 00:03:10 -0500

    Agregando estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ contraseña2.addEventListener("blur",function () {
   var contraseña1 = document.getElementsByClassName("input")[6];
   if (contraseña1.value == contraseña2.value) {
     document.getElementById("button").removeAttribute("disabled");
+    alert("Las contraseñas no coinciden");
   }
   else if (contraseña1.value != contraseña2.value){
     document.getElementById("button").setAttribute("disabled", true)
